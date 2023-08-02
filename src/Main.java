886e9039c08bf9a340cc9e6e626fe6075ab917c6
Ramah Saad <ramahsaad20@gmail.com>
2023-08-02 23:06:30 +0300

    return

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+//this example returns the sum of a methods two parameters
+    static int myMethod2(int x, int y) {
+        return x + y;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(myMethod2(12, 15));
+    }
+}
